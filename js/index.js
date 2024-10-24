542fed512fae88f98e88bcdfb74114819f8bf384
Martseniuk Alina <martseniuk.av@gmail.com>
2024-10-24 17:22:45 +0300

    add

--- js/index.js
+++ js/index.js
@@ -2,5 +2,30 @@
 const temperatureFahrenheit = 100;
 const tempC = (temperatureFahrenheit - 32) / 1.8;
 
+const day = 31;
+const hours = day * 24;
+const minutes = day * 24 * 60;
 
+const health = 100;
+const minusHealth = health - 85;
+
+const shopping = 1500;
+// const discount = shopping - 10% ;
+
+const notWholeNumber = 12.345
+
+const wholeNumber = "46njsr8yj";
+
+const number = 82;
+ 
+    
 console.log(tempC);
+console.log(hours);
+console.log(minutes);
+console.log(health);
+console.log(minusHealth);
+console.log(shopping);
+// console.log(discount);
+console.log(Math.floor(notWholeNumber));
+console.log(Number.parseInt(wholeNumber));
+console.log(Math.sqrt(number));
