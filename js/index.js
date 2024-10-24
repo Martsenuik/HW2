cf591c64d8359ad376f7519cd73e2757db850e26
Martseniuk Alina <martseniuk.av@gmail.com>
2024-10-24 17:58:37 +0300

    add

--- js/index.js
+++ js/index.js
@@ -9,14 +9,22 @@ const minutes = day * 24 * 60;
 const health = 100;
 const minusHealth = health - 85;
 
-const shopping = 1500;
-// const discount = shopping - 10% ;
+let shopping = 1000;
+// const discount = shopping * 0.1;
+// shopping = shopping - discount;
+// shopping = shopping - (shopping * 0.1);
+shopping -= shopping * 0.1;
 
 const notWholeNumber = 12.345
 
 const wholeNumber = "46njsr8yj";
 
 const number = 82;
+
+const num = 567;
+const str = "17px";
+const parsStr = Number.parseInt(str);
+const parsNum = num.toString();
  
     
 console.log(tempC);
@@ -25,7 +33,9 @@ console.log(minutes);
 console.log(health);
 console.log(minusHealth);
 console.log(shopping);
-// console.log(discount);
+// console.log(shopping * 0.1);
 console.log(Math.floor(notWholeNumber));
-console.log(Number.parseInt(wholeNumber));
+console.log(Number.parseInt(wholeNumber ));
 console.log(Math.sqrt(number));
+console.log(parsStr);
+console.log(parsNum);
